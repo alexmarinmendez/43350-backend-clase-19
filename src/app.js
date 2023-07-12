639cb6da82029a5de905cac4d0ddb519e54538c8
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-07-11 19:34:35 -0500

    feat: ejemplo prohibido para desafio de login

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import session from 'express-session'
 import FileStore from 'session-file-store'
+import handlebars from 'express-handlebars'
 
 const app = express()
 const fileStore = FileStore(session)
@@ -13,10 +14,32 @@ app.use(session({
     saveUninitialized: true
 }))
 
+app.engine('handlebars', handlebars.engine())
+app.set('views', './src/views')
+app.set('view engine', 'handlebars')
+
+const auth = (req, res, next) => {
+    if (req.session?.user && req.session.user.username === 'admin@coderhouse.com') {
+        return next()
+    }
+    return res.status(401).json({ status: 'fail', message: 'Auth error' })
+}
+
 app.get('/', (req, res) => res.json({ status: 'success', message: 'Que la fueza te acompañe!' }))
+
+app.get('/products', auth, (req, res) => {
+    //lectura de los prodyctos de la bd
+    const products = [{ name: 'coca cola'}, { name: 'pepsi' }]
+    res.render('products', {
+        username: req.session.user.username,
+        products: products
+    })
+})
+
+
 app.get('/user/profile', (req, res) => {
     const user = {
-        username: 'alexmarinmendez',
+        username: 'admin@coderhouse.com',
         ui_preference: 'dark',
         language: 'es',
         location: 'pe'
